0793c8001f94678c5ecc25f2041088ad6c0e0549
Prohovoch <w1ndoff@yandex.ru>
2026-05-15 21:07:18 +0300

    Репозиторий пользователя

---
+++ Collection/Repository/User/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Collection.Models.User;
+
+namespace Collection.Repository.User
+{
+    // Интерфейс, описывающий контракт исполнение задач репозиторием
+    public interface IUserRepository
+    {
+        Task<IReadOnlyList<UserEntity>> GetAllAsync(CancellationToken ct = default);
+        Task<UserEntity?> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task<UserEntity?> GetByIdWithHubsAsync(Guid id, CancellationToken ct = default);
+        Task<int> DeleteUserAsync(Guid id, CancellationToken ct = default);
+        public void CreateUser(UserEntity user);
+        Task UpdateUserData(Guid id, UserEntity user, CancellationToken ct = default);
+        Task SaveChangesAsync(CancellationToken ct = default);
+    }
+}
