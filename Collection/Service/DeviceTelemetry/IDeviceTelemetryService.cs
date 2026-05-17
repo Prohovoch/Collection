c595bc0af21164b41bc2522716df97e3a8a5bcb4
Prohovoch <w1ndoff@yandex.ru>
2026-05-17 11:12:56 +0700

    Update:
    - сервис по телеметриям.
    - фиксы недочетов.

---
+++ Collection/Service/DeviceTelemetry/IDeviceTelemetryService.cs
@@ -0,0 +1,13 @@
+﻿using Collection.DTO.DeviceTelemetry;
+using System.Collections.Immutable;
+namespace Collection.Service.DeviceTelemetry
+{
+    public interface IDeviceTelemetryService
+    {
+        Task<ImmutableArray<DeviceTelemetryResponseDTO>> GetAllAsync(CancellationToken ct = default);
+        Task<DeviceTelemetryResponseDTO> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task CreateAsync(DeviceTelemetryRequestDTO request, Guid deviceId, CancellationToken ct = default);
+        Task UpdateAsync(Guid id, DeviceTelemtryUpdateDTO update, CancellationToken ct = default);
+        Task DeleteAsync(Guid id, CancellationToken ct = default);
+    }
+}
---
+++ Collection/Service/RobotTelemetry/IRobotTelemetryService.cs
@@ -0,0 +1,14 @@
+﻿using Collection.DTO.RobotTelemetry;
+using System.Collections.Immutable;
+
+namespace Collection.Service.RobotTelemetry
+{
+    public interface IRobotTelemetryService
+    {
+        Task<ImmutableArray<RobotTelemetryResponseDTO>> GetAllAsync(CancellationToken ct = default);
+        Task<RobotTelemetryResponseDTO> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task CreateAsync(RobotTelemetryRequestDTO request, Guid robotId, CancellationToken ct = default);
+        Task UpdateAsync(Guid id, RobotTelemetryUpdateDTO update, CancellationToken ct = default);
+        Task DeleteAsync(Guid id, CancellationToken ct = default);
+    }
+}
